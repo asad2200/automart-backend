914b48ad2cea7ace6e0a370bb2b796325a2112c8
asad2200 <asadjakhavala92@gmail.com>
2021-05-18 19:22:18 +0530

    Models added

--- glass/admin.py
+++ glass/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
-
+from .models import Car, Company, Glass, Model
 # Register your models here.
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+@admin.register(Model)
+class ModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'car']
+
+
+@admin.register(Glass)
+class GlassAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'car', 'model', 'description', 'price']
--- glass/models.py
+++ glass/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=51)
+
+    def __str__(self):
+        return self.name
+
+
+class Car(models.Model):
+    name = models.CharField(max_length=51)
+    company = models.ForeignKey('Company', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Model(models.Model):
+    name = models.CharField(max_length=51)
+    car = models.ForeignKey('Car', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Glass(models.Model):
+    name = models.CharField(max_length=51)
+    car = models.ForeignKey('Car', on_delete=models.CASCADE)
+    model = models.ForeignKey('Model', on_delete=models.CASCADE)
+    description = models.CharField(max_length=200)
+    price = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
